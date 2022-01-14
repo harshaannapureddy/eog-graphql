1caf40edff04fa523070bca2db9624efa14f3d31
harshaannapureddy <vardhanannapureddy@gmail.com>
2022-01-14 03:22:45 -0800

    intial commit

---
+++ src/Redux/Store.ts
@@ -0,0 +1,19 @@
+/* eslint-disable quotes */
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import sagas from './sagas';
+import MetricsReducer from './Reducers/Reducer';
+
+const reducer = combineReducers({ MetricsData: MetricsReducer });
+
+export type IState = ReturnType<typeof reducer>;
+
+export default () => {
+    const composeEnhancers = composeWithDevTools({});
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = applyMiddleware(sagaMiddleware);
+    const store = createStore(reducer, composeEnhancers(middlewares));
+    sagaMiddleware.run(sagas);
+    return store;
+};
